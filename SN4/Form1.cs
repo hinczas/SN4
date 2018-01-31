a195d7af60a1d2076bff0620c9c72b87301802dc
marciner <rafal.marcinek@ipfin.co.uk>
2018-01-31 13:30:44 +0000

    bug fixes / alpha

--- SN4/Form1.cs
+++ SN4/Form1.cs
@@ -29,6 +29,7 @@ namespace SN4
         private int maxy = 260;
         private int minx = 1;
         private int miny = 25;
+        private int level = 0;
 
         public Form1()
         {
@@ -37,28 +38,29 @@ namespace SN4
             Start();
 
             smoothingMode = SmoothingMode.HighQuality;
-            textSmoothing = TextRenderingHint.SystemDefault;
+            textSmoothing = TextRenderingHint.AntiAlias;
             InitializeComponent();
         }
 
         private void Start()
         {
             direction = 'S';
-            int px = rand.Next(minx, maxx);
+            int px = rand.Next(minx, maxx - size);
             px = px - (px % size);
-            int py = rand.Next(miny, maxy);
+            int py = rand.Next(miny, maxy - size);
             py = py - (py % size);
 
             Point head = new Point(px, py);
 
-            int ax = rand.Next(minx, maxx);
+            int ax = rand.Next(minx, maxx - size);
             ax = ax - (ax % size);
-            int ay = rand.Next(miny, maxy);
+            int ay = rand.Next(miny+size, maxy - size);
             ay = ay - (ay % size);
             apple = new Point(ax, ay);
 
             segments = new List<Point>();
             segments.Add(head);
+            sleep = 100;
         }
 
         void keyDown(object sender, KeyEventArgs e)
@@ -106,7 +108,7 @@ namespace SN4
                     tmp.X += size;
                     break;
             }
-            if ((tmp.Y < miny || tmp.Y > maxy-size) || (tmp.X < minx || tmp.X > maxx-size))
+            if ((tmp.Y < miny || tmp.Y > maxy-size*2) || (tmp.X < minx || tmp.X > maxx-size))
             {
                 Start();
             }
@@ -151,11 +153,15 @@ namespace SN4
 
                 segments.Add(tmp);
 
-                int ax = rand.Next(minx, maxx);
+                int ax = rand.Next(minx, maxx-size);
+                //int ax = apple.X + 4;
                 ax = ax - (ax % size);
-                int ay = rand.Next(miny, maxy);
+                int ay = rand.Next(miny+size, maxy-size);
                 ay = ay - (ay % size);
+                //int ay = apple.Y;
                 apple = new Point(ax, ay);
+
+                sleep = sleep == 20 ? 20 : sleep - 1;
             }
         }
 
@@ -165,14 +171,14 @@ namespace SN4
             ResizeRedraw = true;
             Bitmap buffer;
             buffer = new Bitmap(this.Width, this.Height);
-            //Brush aBrush = (Brush)Brushes.Red;
-            //Brush aBrush1 = (Brush)Brushes.Red;
-            //Brush aBrush2 = (Brush)Brushes.Green;
-            //Brush aBrush3 = (Brush)Brushes.LightGreen;
-            Brush aBrush = (Brush)Brushes.LightGray;
-            Brush aBrush1 = (Brush)Brushes.LightGray;
-            Brush aBrush2 = (Brush)Brushes.LightGray;
-            Brush aBrush3 = (Brush)Brushes.LightGray;
+            Brush aBrush = (Brush)Brushes.Red;
+            Brush aBrush1 = (Brush)Brushes.Red;
+            Brush aBrush2 = (Brush)Brushes.Green;
+            Brush aBrush3 = (Brush)Brushes.LightGreen;
+            //Brush aBrush = (Brush)Brushes.LightGray;
+            //Brush aBrush1 = (Brush)Brushes.LightGray;
+            //Brush aBrush2 = (Brush)Brushes.LightGray;
+            //Brush aBrush3 = (Brush)Brushes.LightGray;
             Pen pen = (Pen)Pens.Red;
             //start an async task
             try
@@ -193,26 +199,32 @@ namespace SN4
                     Font drawFont = new Font("Arial", 8);
                     SolidBrush drawBrush = new SolidBrush(Color.Black);
                     PointF drawPoint = new Point(5, 5);
-                    e.Graphics.DrawString(score, drawFont, drawBrush, drawPoint);
+                    gg.DrawString(score, drawFont, drawBrush, drawPoint);
+                    //gg.DrawString("sleep "+sleep+" level "+level, drawFont, drawBrush, drawPoint);
 
-                    gg.DrawLine(new Pen(Color.Black), minx, miny, minx, maxy);
-                    gg.DrawLine(new Pen(Color.Black), minx, miny, maxx, miny);
-                    gg.DrawLine(new Pen(Color.Black), maxx, miny, maxx, maxy);
-                    gg.DrawLine(new Pen(Color.Black), minx, maxy, maxx, maxy);
+                    gg.DrawLine(new Pen(Color.Gray), minx-1, miny-1, minx-1, maxy+1);                   // Vertical left gray
+                    gg.DrawLine(new Pen(Color.Black), minx, miny, minx, maxy);                          // Vertical left black
+                    gg.DrawLine(new Pen(Color.Gray), minx-1, miny-1, maxx+1, miny-1);                   // Horizontal top gray
+                    gg.DrawLine(new Pen(Color.LightGray), minx - 1, miny - 2, maxx + 1, miny - 2);      // Horizontal top light gray
+                    gg.DrawLine(new Pen(Color.Black), minx, miny, maxx, miny);                          // Horizontal top black
+                    gg.DrawLine(new Pen(Color.Gray), maxx + 1, miny - 1, maxx + 1, maxy + 1);           // Vertical right gray
+                    gg.DrawLine(new Pen(Color.Black), maxx, miny, maxx, maxy);                          // Vertical right black
+                    gg.DrawLine(new Pen(Color.Gray), minx-1, maxy+1, maxx+1, maxy+1);                   // Horizontal bottom gray
+                    gg.DrawLine(new Pen(Color.Black), minx, maxy, maxx, maxy);                          // Horizontal bottom black
                     gg.FillEllipse(aBrush1, new RectangleF(apple, new Size(4, 4)));
-                    gg.DrawEllipse(new Pen(Color.LightGray), new RectangleF(apple, new Size(4, 4)));
+                    gg.DrawEllipse(new Pen(Color.Black), new RectangleF(apple, new Size(4, 4)));
 
                     for (int i = 0; i < segments.Count; i++)
                     {
                         Point segment = segments[i];
                         aBrush = i == segments.Count - 1 ? aBrush2 : aBrush3;
                         gg.FillEllipse(aBrush, new RectangleF(segment, new Size(5, 5)));
-                        gg.DrawEllipse(new Pen(Color.LightGray), new RectangleF(segment, new Size(5, 5)));
+                        gg.DrawEllipse(new Pen(Color.Black), new RectangleF(segment, new Size(5, 5)));
                     }
 
                     OnApple();
 
-                    if (counter == sleep)
+                    if (counter >= sleep)
                     {
                         MoveSegments(direction);
                         counter = 0;
